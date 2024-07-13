951e552c9429da899eb9befcc213c3609b4e3b2a
Tolojanahary <79153294+AmazingTool777@users.noreply.github.com>
2024-07-13 22:06:57 +0300

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { Outlet } from 'react-router-dom';
+
+import RootLayout from './layouts/RootLayout';
+
+function App() {
+  return (
+    <RootLayout>
+      <Outlet />
+    </RootLayout>
+  );
+}
+
+export default App;
